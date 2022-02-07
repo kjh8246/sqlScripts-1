6a243b11ed5059374683e16a1d3e21a7a7210101
kimsohee <koreait.2021@gmail.com>
2022-02-07 15:39:47 +0900

    day3 제약조건2

--- oracle/day3_0207(3).sql
+++ oracle/day3_0207(3).sql
@@ -5,7 +5,7 @@
 -- 중요3: 고객과 상품테이블에 없는 CUSTOM_ID,PCODE 컬럼 값을 사용하면 부정확한 데이터
 --		ㄴ 외래키 컬럼을 정합니다.(다른테이블에서 값을 참조합니다.NULL 허용도 안합니다.)
 
--- 위의 NOT NULL, 기본키 , 외래키는 대표적인 제약조건(CONSTRAINT) 설정입니다.
+-- 위의 NOT NULL, 기본키 , 외래키는 대표적인 *제약조건(CONSTRAINT)* 설정입니다.
 -- 제약조건을 설정한 테이블로 생성해 봅니다.
 -- 고객 테이블 
 CREATE TABLE tbl_custom# (
@@ -39,4 +39,73 @@ CREATE TABLE tbl_buy#(
 	CONSTRAINT fk_pcode FOREIGN KEY (pcode)
 		REFERENCES tbl_product#(pcode)
 	-- 외래키로 설정될 수 있는 컬럼은 기본키 또는 unique(유일한) 에 대해서만 가능합니다.	
+	-- 외래키 컬럼은 참조(REFERENCES)테이블의	 컬럼값 중 하나로 저장할 수 있습니다.
 );
+
+SELECT * FROM "TBL_CUSTOM#" tc ;
+-- 데이터의 추가 : INSERT 
+INSERT INTO IDEV.TBL_CUSTOM#(CUSTOM_ID, NAME, EMAIL, AGE, REG_DATE)
+VALUES('mina012', '김미나', 'kimm@gmail.com', 20, sysdate);
+INSERT INTO IDEV.TBL_CUSTOM#(CUSTOM_ID, NAME, EMAIL, AGE, REG_DATE)
+VALUES('hongGD', '홍길동', 'gil@korea.com', 32, sysdate);
+INSERT INTO IDEV.TBL_CUSTOM#(CUSTOM_ID, NAME, EMAIL, AGE, REG_DATE)
+VALUES('twice', '박모모', 'momo@daum.net', 29, sysdate);
+INSERT INTO IDEV.TBL_CUSTOM#(CUSTOM_ID, NAME, EMAIL, AGE, REG_DATE)
+VALUES('wonder', '이나나', 'lee@naver.com', 40, sysdate);
+-- 중요1 : custom 테이블의 행(row)데이터를 구분한 CUSTOM_ID ,PCODE 는 중복되면 안됩니다.
+INSERT INTO IDEV.TBL_CUSTOM#(CUSTOM_ID, NAME, EMAIL, AGE, REG_DATE)
+VALUES('wonder', '최모모', 'choi@naver.com', 33, sysdate);
+
+SELECT * FROM "TBL_PRODUCT#" tp ;
+INSERT INTO IDEV.TBL_PRODUCT#(PCODE, CATEGORY, PNAME, PRICE)
+VALUES('IPAD011', 'A1', '아이패드10', 880000);
+INSERT INTO IDEV.TBL_PRODUCT#(PCODE, CATEGORY, PNAME, PRICE)
+VALUES('DOWON123a', 'B1', '동원참치선물세트', 54000);
+INSERT INTO IDEV.TBL_PRODUCT#(PCODE, CATEGORY, PNAME, PRICE)
+VALUES('dk_143', 'A2', '모션데스크', 234500);
+-- 중요2: 예를들면  PRICE , QUANTITY 등의 컬럼은 꼭 필수 데이터로 저장되어야 합니다.
+INSERT INTO IDEV.TBL_PRODUCT#(PCODE, CATEGORY, PNAME)
+VALUES('dk_1433', 'A2', '자동모션데스크');
+
+
+-- 오라클에서는 일련번호를 자동으로 증가해서 사용하게 할수 있는 시퀀스 기능이 있스빈다.
+CREATE SEQUENCE tbl_buy_seq;
+-- 한번만들어져서 사용하던 시퀀스는 값을 되돌릴 수 없습니다.
+
+--시퀀스 새로 만들고 싶다면 삭제하고 다시 만드세요.
+DROP SEQUENCE tbl_buy_seq ;
+--		테이블의 데이터도 삭제하세요. 그리고 다시 시퀀스 생성/데이터 추가하세요.
+
+SELECT * FROM TBL_BUY# tb ;
+
+-- 김미나가 아이패드 1개를 어제 구매
+INSERT INTO IDEV.TBL_BUY#(BUY_SEQ,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES(tbl_buy_seq.nextval,'mina012', 'IPAD011', 1, '2022-02-06');
+-- 홍길동이 아이패드 2개를 오늘 구매
+INSERT INTO IDEV.TBL_BUY#(BUY_SEQ,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES(tbl_buy_seq.nextval,'hongGD', 'IPAD011', 2, sysdate);
+-- 이나나가 참치선물세트 3개를 어제 구매
+INSERT INTO IDEV.TBL_BUY#(BUY_SEQ,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES(tbl_buy_seq.nextval,'wonder', 'DOWON123a', 3, '2022-02-06');
+-- 김미나가 모션데스크 1개를 오늘 구매
+INSERT INTO IDEV.TBL_BUY#(BUY_SEQ,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES(tbl_buy_seq.nextval,'mina012', 'dk_143',1, sysdate);
+-- 박모모가 참치선물세트 2개를 오늘 구매
+INSERT INTO IDEV.TBL_BUY#(BUY_SEQ,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES(tbl_buy_seq.nextval,'twice', 'DOWON123a', 2, '2022-02-07');
+
+-- 중요3: 고객과 상품테이블에 없는 CUSTOM_ID,PCODE 컬럼 값을 사용하면 부정확한 데이터
+-- 외래키로 설정된 custom_id는 tbl_custom# 테이블을 참조합니다. 참조테이블의
+-- custom_id 컬럼값에 없는것을 저장할 수 없습니다. (외래키 위반,무결성 제약조건 위반)
+INSERT INTO IDEV.TBL_BUY#(BUY_SEQ,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES(tbl_buy_seq.nextval,'min012', 'dk_143',1, sysdate);
+-- 외래키로 설정된 pcode는 tbl_product# 테이블을 참조합니다. 참조테이블의
+-- pcode 컬럼값에 없는것을 저장할 수 없습니다. (외래키 위반,무결성 제약조건 위반)
+INSERT INTO IDEV.TBL_BUY#(BUY_SEQ,CUSTOM_ID, PCODE, QUANTITY, BUY_DATE)
+VALUES(tbl_buy_seq.nextval,'mina012', 'd_14',1, sysdate);
+
+
+
+
+
+
